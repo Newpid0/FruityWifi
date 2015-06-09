2151033e40b39271bcb921c1a2f2fee179db79c6
Newpid0 <f8lerror+git@gmail.com>
2015-06-09 17:25:43 -0400

    Create reboot.php

---
+++ FruityWifi/www/reboot.php
@@ -0,0 +1,12 @@
+<?php
+
+
+include "login_check.php";
+include_once "/usr/share/fruitywifi/www/config/config.php";
+header("Expires: Mon, 1 Jan 1900 00:00:00 GMT");
+header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
+header("Cache-Control: no-store, no-cache, must-revalidate");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+   exec('sudo reboot');
+?>
